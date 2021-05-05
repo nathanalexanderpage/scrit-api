7ff80dd6255f01e818a1f345aaef15f09f69042d
nathanalexanderpage <nathanalexanderpage@gmail.com>
2021-05-05 01:18:13 -0700

    most simple routes added

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,8 @@ function getClient () {
 
 app.use(express.json());
 
+// TODO: check for req.body keys for field entries (currently, any nullable columns will simply be NULL if req.body.nullableField is undefined)
+
 app.get("/", (req, res) => {
 	res.status(200);
 	res.end("Scrit ping successful. Your hero's journey begins.");
@@ -75,7 +77,7 @@ app.post("/projects", async (req, res) => {
 		const result = await db.query(sql, [req.body.name, req.body.description]);
 		console.log(result)
 
-		// make a timeline entry automatically
+		// TODO: make a timeline entry automatically
 
 		if (result.rowCount > 0) {
 			res.status(201);
@@ -143,7 +145,7 @@ app.delete("/projects/:id", async (req, res) => {
 		`;
 
 		db.connect();
-		const result = await db.query(sql, [req.body.id]);
+		const result = await db.query(sql, [req.params.id]);
 		console.log(result);
 
 		if (result.rowCount > 0) {
@@ -236,6 +238,71 @@ app.post("/notes", async (req, res) => {
 	}
 });
 
+app.put("/notes", async (req, res) => {
+	const db = getClient();
+
+	try {
+		const sql = `
+			UPDATE
+				project_notes
+			SET
+				project_id = $2,
+				name = $3,
+				description = $4
+			WHERE
+				id = $1;
+		`;
+
+		db.connect();
+		const result = await db.query(sql, [req.body.id, req.body.projectId, req.body.name, req.body.description]);
+		console.log(result)
+
+		if (result.rowCount > 0) {
+			res.status(201);
+		} else {
+			res.status(400);
+		}
+
+		res.send();
+	} catch (err) {
+		res.status(500);
+		res.json({error: err.message || err});
+	} finally {
+		db.end();
+	}
+});
+
+app.delete("/notes/:id", async (req, res) => {
+	const db = getClient();
+
+	try {
+		const sql = `
+			DELETE
+			FROM
+				project_notes
+			WHERE
+				id = $1;
+		`;
+
+		db.connect();
+		const result = await db.query(sql, [req.params.id]);
+		console.log(result);
+
+		if (result.rowCount > 0) {
+			res.status(204);
+		} else {
+			res.status(400);
+		}
+
+		res.send();
+	} catch (err) {
+		res.status(500);
+		res.json({error: err.message || err});
+	} finally {
+		db.end();
+	}
+});
+
 app.get("/projects/:id/timelines", async (req, res) => {
 	const db = getClient();
 
@@ -307,6 +374,71 @@ app.post("/timelines", async (req, res) => {
 	}
 });
 
+app.put("/timelines", async (req, res) => {
+	const db = getClient();
+
+	try {
+		const sql = `
+			UPDATE
+				timelines
+			SET
+				project_id = $2,
+				name = $3,
+				description = $4
+			WHERE
+				id = $1;
+		`;
+
+		db.connect();
+		const result = await db.query(sql, [req.body.id, req.body.projectId, req.body.name, req.body.description]);
+		console.log(result)
+
+		if (result.rowCount > 0) {
+			res.status(201);
+		} else {
+			res.status(400);
+		}
+
+		res.send();
+	} catch (err) {
+		res.status(500);
+		res.json({error: err.message || err});
+	} finally {
+		db.end();
+	}
+});
+
+app.delete("/timelines/:id", async (req, res) => {
+	const db = getClient();
+
+	try {
+		const sql = `
+			DELETE
+			FROM
+				timelines
+			WHERE
+				id = $1;
+		`;
+
+		db.connect();
+		const result = await db.query(sql, [req.params.id]);
+		console.log(result);
+
+		if (result.rowCount > 0) {
+			res.status(204);
+		} else {
+			res.status(400);
+		}
+
+		res.send();
+	} catch (err) {
+		res.status(500);
+		res.json({error: err.message || err});
+	} finally {
+		db.end();
+	}
+});
+
 app.get("/projects/:id/meta-locations", async (req, res) => {
 	const db = getClient();
 
@@ -373,6 +505,71 @@ app.post("/meta-locations", async (req, res) => {
 			res.status(500);
 			res.json({error: err.message || err});
 		}
+	} finally {
+		db.end();
+	}
+});
+
+app.put("/meta-locations", async (req, res) => {
+	const db = getClient();
+
+	try {
+		const sql = `
+			UPDATE
+				locations_meta
+			SET
+				project_id = $2,
+				name = $3,
+				description = $4
+			WHERE
+				id = $1;
+		`;
+
+		db.connect();
+		const result = await db.query(sql, [req.body.id, req.body.projectId, req.body.name, req.body.description]);
+		console.log(result)
+
+		if (result.rowCount > 0) {
+			res.status(201);
+		} else {
+			res.status(400);
+		}
+
+		res.send();
+	} catch (err) {
+		res.status(500);
+		res.json({error: err.message || err});
+	} finally {
+		db.end();
+	}
+});
+
+app.delete("/meta-locations/:id", async (req, res) => {
+	const db = getClient();
+
+	try {
+		const sql = `
+			DELETE
+			FROM
+				locations_meta
+			WHERE
+				id = $1;
+		`;
+
+		db.connect();
+		const result = await db.query(sql, [req.params.id]);
+		console.log(result);
+
+		if (result.rowCount > 0) {
+			res.status(204);
+		} else {
+			res.status(400);
+		}
+
+		res.send();
+	} catch (err) {
+		res.status(500);
+		res.json({error: err.message || err});
 	} finally {
 		db.end();
 	}
@@ -484,6 +681,72 @@ app.post("/locations", async (req, res) => {
 	}
 });
 
+app.put("/locations", async (req, res) => {
+	const db = getClient();
+
+	try {
+		const sql = `
+			UPDATE
+				locations
+			SET
+				meta_location_id = $2,
+				name_for_writer_reference = $3,
+				name_canon = $4,
+				description = $5
+			WHERE
+				id = $1;
+		`;
+
+		db.connect();
+		const result = await db.query(sql, [req.body.id, req.body.metaLocationId, req.body.nameForWriterReference, req.body.nameCanon, req.body.description]);
+		console.log(result)
+
+		if (result.rowCount > 0) {
+			res.status(201);
+		} else {
+			res.status(400);
+		}
+
+		res.send();
+	} catch (err) {
+		res.status(500);
+		res.json({error: err.message || err});
+	} finally {
+		db.end();
+	}
+});
+
+app.delete("/locations/:id", async (req, res) => {
+	const db = getClient();
+
+	try {
+		const sql = `
+			DELETE
+			FROM
+				locations
+			WHERE
+				id = $1;
+		`;
+
+		db.connect();
+		const result = await db.query(sql, [req.params.id]);
+		console.log(result);
+
+		if (result.rowCount > 0) {
+			res.status(204);
+		} else {
+			res.status(400);
+		}
+
+		res.send();
+	} catch (err) {
+		res.status(500);
+		res.json({error: err.message || err});
+	} finally {
+		db.end();
+	}
+});
+
 app.get("/projects/:id/meta-characters", async (req, res) => {
 	const db = getClient();
 
@@ -491,14 +754,13 @@ app.get("/projects/:id/meta-characters", async (req, res) => {
 		const sql = `
 			SELECT
 				id,
-				meta_location_id,
-				name_for_writer_reference,
-				name_canon,
+				project_id,
+				name,
 				description
 			FROM
 				characters_meta
 			WHERE
-				meta_location_id = $1;
+				project_id = $1;
 		`;
 
 		db.connect();
@@ -551,6 +813,71 @@ app.post("/meta-characters", async (req, res) => {
 			res.status(500);
 			res.json({error: err.message || err});
 		}
+	} finally {
+		db.end();
+	}
+});
+
+app.put("/meta-characters", async (req, res) => {
+	const db = getClient();
+
+	try {
+		const sql = `
+			UPDATE
+				characters_meta
+			SET
+				project_id = $2,
+				name = $3,
+				description = $4
+			WHERE
+				id = $1;
+		`;
+
+		db.connect();
+		const result = await db.query(sql, [req.body.id, req.body.projectId, req.body.name, req.body.description]);
+		console.log(result)
+
+		if (result.rowCount > 0) {
+			res.status(201);
+		} else {
+			res.status(400);
+		}
+
+		res.send();
+	} catch (err) {
+		res.status(500);
+		res.json({error: err.message || err});
+	} finally {
+		db.end();
+	}
+});
+
+app.delete("/meta-characters/:id", async (req, res) => {
+	const db = getClient();
+
+	try {
+		const sql = `
+			DELETE
+			FROM
+				characters_meta
+			WHERE
+				id = $1;
+		`;
+
+		db.connect();
+		const result = await db.query(sql, [req.params.id]);
+		console.log(result);
+
+		if (result.rowCount > 0) {
+			res.status(204);
+		} else {
+			res.status(400);
+		}
+
+		res.send();
+	} catch (err) {
+		res.status(500);
+		res.json({error: err.message || err});
 	} finally {
 		db.end();
 	}
@@ -624,6 +951,75 @@ app.get("/projects/:id/characters", async (req, res) => {
 	}
 });
 
+app.get("/characters/:id/events", async (req, res) => {
+	const db = getClient();
+
+	try {
+		const sql = `
+			SELECT
+				events.id,
+				events.timeline_id,
+				events.name_for_writer_reference,
+				events.name_canon,
+				events.datetime_start,
+				events.datetime_end,
+				events.description
+			FROM
+				events
+			JOIN
+				characters_events ON events.id = characters_events.event_id
+			WHERE
+				characters_events.event_id = $1;
+		`;
+
+		db.connect();
+		const result = await db.query(sql, [req.params.id]);
+		console.log(result.rows)
+
+		res.status(200);
+		res.json(result.rows);
+	} catch (err) {
+		res.json({error: err.message || err});
+	} finally {
+		db.end();
+	}
+});
+
+app.get("/events/:id/characters", async (req, res) => {
+	const db = getClient();
+
+	try {
+		const sql = `
+			SELECT
+				characters.id,
+				characters.meta_character_id,
+				characters.name_for_writer_reference,
+				characters.name_given,
+				characters.name_goes_by,
+				characters.description,
+				characters.birthyear,
+				characters.birthplace
+			FROM
+				characters
+			JOIN
+				characters_events ON characters_events.character_id = characters.id
+			WHERE
+				characters_events.event_id = $1;
+		`;
+
+		db.connect();
+		const result = await db.query(sql, [req.params.id]);
+		console.log(result.rows)
+
+		res.status(200);
+		res.json(result.rows);
+	} catch (err) {
+		res.json({error: err.message || err});
+	} finally {
+		db.end();
+	}
+});
+
 app.post("/characters", async (req, res) => {
 	const db = getClient();
 
@@ -669,6 +1065,86 @@ app.post("/characters", async (req, res) => {
 			res.status(500);
 			res.json({error: err.message || err});
 		}
+	} finally {
+		db.end();
+	}
+});
+
+app.put("/characters", async (req, res) => {
+	const db = getClient();
+
+	try {
+		const sql = `
+			UPDATE
+				characters
+			SET
+				meta_character_id = $2,
+				name_for_writer_reference = $3,
+				name_given = $4,
+				name_goes_by = $5,
+				description = $6,
+				birthyear = $7,
+				birthplace = $8
+			WHERE
+				id = $1;
+		`;
+
+		db.connect();
+
+		const result = await db.query(sql, [
+			req.body.id,
+			req.body.metaCharacterId,
+			req.body.nameForWriterReference,
+			req.body.nameGiven,
+			req.body.nameGoesBy,
+			req.body.description,
+			req.body.birthyear,
+			req.body.birthplace,
+		]);
+
+		console.log(result)
+
+		if (result.rowCount > 0) {
+			res.status(201);
+		} else {
+			res.status(400);
+		}
+
+		res.send();
+	} catch (err) {
+		res.status(500);
+		res.json({error: err.message || err});
+	} finally {
+		db.end();
+	}
+});
+
+app.delete("/characters/:id", async (req, res) => {
+	const db = getClient();
+
+	try {
+		const sql = `
+			DELETE
+			FROM
+				characters
+			WHERE
+				id = $1;
+		`;
+
+		db.connect();
+		const result = await db.query(sql, [req.params.id]);
+		console.log(result);
+
+		if (result.rowCount > 0) {
+			res.status(204);
+		} else {
+			res.status(400);
+		}
+
+		res.send();
+	} catch (err) {
+		res.status(500);
+		res.json({error: err.message || err});
 	} finally {
 		db.end();
 	}
@@ -788,6 +1264,84 @@ app.post("/events", async (req, res) => {
 	}
 });
 
+app.put("/events", async (req, res) => {
+	const db = getClient();
+
+	try {
+		const sql = `
+			UPDATE
+				events
+			SET
+				timeline_id = $2,
+				name_for_writer_reference = $3,
+				name_canon = $4,
+				datetime_start = $5,
+				datetime_end = $6,
+				description = $7
+			WHERE
+				id = $1;
+		`;
+
+		db.connect();
+
+		const result = await db.query(sql, [
+			req.body.id,
+			req.body.timelineId,
+			req.body.nameForWriterReference,
+			req.body.nameCanon,
+			req.body.datetimeStart,
+			req.body.datetimeEnd,
+			req.body.description,
+		]);
+
+		console.log(result)
+
+		if (result.rowCount > 0) {
+			res.status(201);
+		} else {
+			res.status(400);
+		}
+
+		res.send();
+	} catch (err) {
+		res.status(500);
+		res.json({error: err.message || err});
+	} finally {
+		db.end();
+	}
+});
+
+app.delete("/events/:id", async (req, res) => {
+	const db = getClient();
+
+	try {
+		const sql = `
+			DELETE
+			FROM
+				events
+			WHERE
+				id = $1;
+		`;
+
+		db.connect();
+		const result = await db.query(sql, [req.params.id]);
+		console.log(result);
+
+		if (result.rowCount > 0) {
+			res.status(204);
+		} else {
+			res.status(400);
+		}
+
+		res.send();
+	} catch (err) {
+		res.status(500);
+		res.json({error: err.message || err});
+	} finally {
+		db.end();
+	}
+});
+
 app.post("/events/characters", async (req, res) => {
 	const db = getClient();
 
@@ -828,6 +1382,42 @@ app.post("/events/characters", async (req, res) => {
 	}
 });
 
+app.delete("/events/:eventId/characters/:characterId", async (req, res) => {
+	const db = getClient();
+
+	try {
+		const sql = `
+			DELETE
+			FROM
+				characters_events
+			WHERE
+				event_id = $1
+				AND
+				character_id = $2;
+		`;
+
+		db.connect();
+		const result = await db.query(sql, [
+			req.params.eventId,
+			req.params.characterId,
+		]);
+		console.log(result);
+
+		if (result.rowCount > 0) {
+			res.status(204);
+		} else {
+			res.status(400);
+		}
+
+		res.send();
+	} catch (err) {
+		res.status(500);
+		res.json({error: err.message || err});
+	} finally {
+		db.end();
+	}
+});
+
 app.get("/projects/:id/event-tags", async (req, res) => {
 	const db = getClient();
 
@@ -842,7 +1432,7 @@ app.get("/projects/:id/event-tags", async (req, res) => {
 			FROM
 				event_tags
 			WHERE
-				parent_event_id = $1;
+				project_id = $1;
 		`;
 
 		db.connect();
@@ -902,22 +1492,97 @@ app.post("/event-tags", async (req, res) => {
 	}
 });
 
+app.put("/event-tags", async (req, res) => {
+	const db = getClient();
+
+	try {
+		const sql = `
+			UPDATE
+				event_tags
+			SET
+				project_id = $2,
+				parent_event_id = $3,
+				name = $4,
+				description = $5
+			WHERE
+				id = $1;
+		`;
+
+		db.connect();
+
+		const result = await db.query(sql, [
+			req.body.id,
+			req.body.projectId,
+			req.body.parentEventId,
+			req.body.name,
+			req.body.description,
+		]);
+
+		console.log(result)
+
+		if (result.rowCount > 0) {
+			res.status(201);
+		} else {
+			res.status(400);
+		}
+
+		res.send();
+	} catch (err) {
+		res.status(500);
+		res.json({error: err.message || err});
+	} finally {
+		db.end();
+	}
+});
+
+app.delete("/event-tags/:id", async (req, res) => {
+	const db = getClient();
+
+	try {
+		const sql = `
+			DELETE
+			FROM
+				event_tags
+			WHERE
+				id = $1;
+		`;
+
+		db.connect();
+		const result = await db.query(sql, [req.params.id]);
+		console.log(result);
+
+		if (result.rowCount > 0) {
+			res.status(204);
+		} else {
+			res.status(400);
+		}
+
+		res.send();
+	} catch (err) {
+		res.status(500);
+		res.json({error: err.message || err});
+	} finally {
+		db.end();
+	}
+});
+
 app.get("/events/:id/event-tags", async (req, res) => {
 	const db = getClient();
 
 	try {
 		const sql = `
 			SELECT
-				id,
-				parent_event_id,
-				name,
-				description
+				event_tags.id,
+				event_tags.project_id,
+				event_tags.parent_event_id,
+				event_tags.name,
+				event_tags.description
 			FROM
 				event_tags
 			JOIN
 				events_event_tags
 			ON
-				event_tags.id = events_event_tags.event_id
+				event_tags.id = events_event_tags.event_tag_id
 			WHERE
 				events_event_tags.event_id = $1;
 		`;
